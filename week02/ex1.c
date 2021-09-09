e640590252c4e30d79d5910e6002ec9d9afa7133
Igor Alentev <m8dotpie@Igors-MacBook-Pro.local>
2021-09-09 15:17:40 +0300

    Tmp fix

--- week02/ex1.c
+++ week02/ex1.c
@@ -9,5 +9,5 @@ int main() {
 
     printf("Integer size: %lu\nInteger max value: %d\n", sizeof(i), i);
     printf("Float size: %lu\nFloat max value: %f\n", sizeof(f), f);
-    printf("Double size: %lu\nDouble max value: %lf\n", sizeof(f), f);
+    printf("Double size: %lu\nDouble max value: %f\n", sizeof(f), f);
 }
